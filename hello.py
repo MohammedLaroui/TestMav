78fb3195817cf6b4a6eb9b580475ee752517de69
MohammedL <50454062+MohammedLaroui@users.noreply.github.com>
2022-03-05 17:25:58 +0100

    dqdqzdqzdqzdzqd

--- hello.py
+++ hello.py
@@ -1,9 +1,1 @@
 print("Welcome")
-print("first change")
-print("second change")
-print("second change")
-print("second change")
-print("second change")
-print("second change")
-print("second change")
-print("second change")qdqzdqz
